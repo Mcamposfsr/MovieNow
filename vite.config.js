739abe4bd948b47afb4434955bc43426f8b3557f
Matheus Campos <m.campos612003@gmail.com>
2025-06-03 10:24:25 -0300

    Iniciando o projeto

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  resolve:{
+    alias:{"@":"/src"},
+    extensions:[".js",".jsx"]
+    
+  },
+  server:{
+    port:"3000",
+    open:true
+  }
   
 })
